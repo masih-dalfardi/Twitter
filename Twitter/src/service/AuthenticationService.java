8d9053caef95950fe3e4bd4f7fc0c098e39a1f22
smaresalat <58745774+smaresalat@users.noreply.github.com>
2021-12-07 16:15:54 +0330

    [Feature] The Authentication service

--- Twitter/src/service/AuthenticationService.java
+++ Twitter/src/service/AuthenticationService.java
@@ -1,4 +1,44 @@
 package service;
-//testing
+
+import dao.ClientDao;
+import model.Client;
+import java.util.Date;
+import java.util.Optional;
+
+/**
+ * The Authentication service.
+ */
 public class AuthenticationService {
-}
+    private final static int MAX_BIO_LENGTH = 256;
+    private final ClientDao clientDao = new ClientDao();
+
+    /**
+     * Sign in int.
+     *
+     * @param username the username
+     * @param password the password
+     * @return the int
+     */
+    public int signIn(String username, String password) {
+        Optional<Client> optionalClient = clientDao.searchByUsername(username);
+        if (optionalClient.isPresent()) {
+            Client client = optionalClient.get();
+            if (authenticateClient(client, password) == 0) {
+                clientDao.addToLogInClients(client);
+                return 0;
+            } else {
+                // wrong password
+                return -1;
+            }
+        } else {
+            // wrong username
+            return 1;
+        }
+    }
+
+    private int authenticateClient(Client client, String password) {
+        if (client.getPassword().equals(password))
+            return 0;
+        else
+            return -1;
+    }
