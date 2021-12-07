8a275f19dae42a07d4c7f5378f65c55ed764259d
smaresalat <58745774+smaresalat@users.noreply.github.com>
2021-12-07 21:11:06 +0330

    [Feature] Sign up method added

--- Twitter/src/service/AuthenticationService.java
+++ Twitter/src/service/AuthenticationService.java
@@ -42,3 +42,30 @@ public class AuthenticationService {
         else
             return -1;
     }
+    /**
+     * Signup int.
+     *
+     * @param username  the username
+     * @param password  the password
+     * @param firstname the firstname
+     * @param lastname  the lastname
+     * @param birthdate the birthdate
+     * @param bio       the bio
+     * @return the int
+     */
+    public int signup(String username, String password, String firstname, String lastname, Date birthdate, String bio) {
+
+        // maximum length of bio!
+        if (bio.length() > MAX_BIO_LENGTH)
+            return 1;
+        Optional<Client> optionalClient = clientDao.searchByUsername(username);
+        if (optionalClient.isEmpty()) {
+            Client client = new Client(firstname, lastname, username,
+                    password, birthdate, new Date(), bio);
+            return clientDao.addToTotalClients(client);
+        } else {
+            // username is already exists!
+            return -1;
+        }
+    }
+}
