ea6d93487324837b11740df271256d691c05bd92
masih-dalfardi <58389776+masih-dalfardi@users.noreply.github.com>
2021-12-09 11:26:43 +0330

    feature [remove and add tweet method added]

--- Twitter/src/dao/TweetDao.java
+++ Twitter/src/dao/TweetDao.java
@@ -1,4 +1,35 @@
 package dao;
 
+import model.Client;
+import model.Tweet;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Optional;
+
+/**
+ * The Tweet DAO
+ */
 public class TweetDao {
-}
+
+    /**
+     * Add tweet
+     *
+     * @param tweet the tweet
+     * @return the int
+     */
+    public int addTweet(Tweet tweet) {
+        Client sender = tweet.getSender();
+        return sender.addTweet(tweet);
+    }
+
+    /**
+     * Remove tweet int.
+     *
+     * @param tweet the tweet
+     * @return the int
+     */
+    public int removeTweet(Tweet tweet) {
+        Client sender = tweet.getSender();
+        return sender.removeTweet(tweet);
+    }
