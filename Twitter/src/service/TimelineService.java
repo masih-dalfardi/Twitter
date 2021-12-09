2a608fd3f52511e196a2fcf2fb28d24ba28ad07f
smaresalat <58745774+smaresalat@users.noreply.github.com>
2021-12-09 11:46:41 +0330

    [Feature] TimeLineService class, constructor and methods

--- Twitter/src/service/TimelineService.java
+++ Twitter/src/service/TimelineService.java
@@ -1,4 +1,35 @@
 package service;
 
-public class TimelineService {
+import display.ConsoleDisplay;
+import model.Client;
+import model.Tweet;
+
+import java.util.ArrayList;
+
+/**
+ * The Time line service.
+ */
+public class TimeLineService {
+    private final ConsoleDisplay consoleDisplay = new ConsoleDisplay();
+
+    /**
+     * Show all followings tweets.
+     *
+     * @param client the client
+     */
+    public void showAllFollowingsTweets(Client client) {
+        ArrayList<Client> followings = client.getFollowings();
+        for (Client following: followings) {
+            System.out.println(following.getUsername() + ":");
+            showFollowingTweet(following);
+            System.out.println("---------------------");
+        }
+    }
+
+    private void showFollowingTweet(Client following) {
+        for (Tweet tweet: following.getTweets()) {
+            consoleDisplay.displayTweet(tweet);
+        }
+    }
+
 }
