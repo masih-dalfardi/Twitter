0454fee2fe590bd1743d8c6d4c6e188a376f362c
masih-dalfardi <58389776+masih-dalfardi@users.noreply.github.com>
2021-12-20 11:59:43 +0330

    feature [start server with killer thread method uploaded]

--- Twitter/src/server/MainServer.java
+++ Twitter/src/server/MainServer.java
@@ -1,5 +1,44 @@
-package server;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
-public class MainServer {
 
-}
+public class Server
+{
+    private final int port = 8080;
+    private ServerSocket server;
+    private Socket socket;
+    private ObjectOutputStream writer;
+    private DataInputStream reader;
+
+    // number of connections -> primary key for clients
+    private static int connection;
+
+    // a temp map to help connecting USER to SOCKET
+    private static Map<Integer, Socket> connections = new HashMap<>();
+
+    // to kill client thread when app gets closed
+    private static killer = new HashMap<>();
+
+    public void setKiller(TaskListener listener)
+    {
+        killer.put(connection, listener);
+    }
+
+    public static Map<Integer, TaskListener> getKiller()
+    {
+        return killer;
+    }
+
+    // kill a thread
+    public void kill(int number) throws IOException
+    {
+        killer.get(number).kill();
+        killer.remove(number);
+
+        connections.get(number).close();
+        connections.remove(number);
+    }
