7aaa0f6e4e16caf2e4c2a71b4efb16efa289499e
masih-dalfardi <58389776+masih-dalfardi@users.noreply.github.com>
2021-12-20 12:02:16 +0330

    feature [constructor and some methods added]

--- Twitter/src/server/TaskHandler.java
+++ Twitter/src/server/TaskHandler.java
@@ -1,4 +1,28 @@
 package server;
 
-public class TaskHandler {
-}
+import data.Tweet;
+import data.User;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.time.LocalDate;
+
+/**
+ * this class handles client's message
+ */
+
+public class TaskHandler
+{
+    private JSONObject mapper;
+    private ObjectOutputStream writer;
+    private DataInputStream reader;
+
+    public TaskHandler(JSONObject mapper, ObjectOutputStream writer, DataInputStream reader)
+    {
+        this.mapper = mapper;
+        this.writer = writer;
+        this.reader = reader;
+    }
