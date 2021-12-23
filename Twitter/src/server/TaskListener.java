e183c98d0b1297dae8ea44fac268650abbefe2c1
masih-dalfardi <58389776+masih-dalfardi@users.noreply.github.com>
2021-12-23 10:11:19 +0330

    feature [some fields and constructor added]

--- Twitter/src/server/TaskListener.java
+++ Twitter/src/server/TaskListener.java
@@ -1,4 +1,37 @@
-package server;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 
-public class TaskListener {
-}
+/**
+ * this runnable waits until a message come from client
+ */
+
+public class TaskListener implements Runnable
+{
+    private ObjectOutputStream writer;
+
+    public ObjectOutputStream getWriter()
+    {
+        return writer;
+    }
+
+    public DataInputStream getReader()
+    {
+        return reader;
+    }
+
+    private DataInputStream reader;
+    private String message;
+    private JSONObject mapper;
+    private boolean continues = true;
+
+    public TaskListener(ObjectOutputStream writer, DataInputStream reader)
+    {
+        this.writer = writer;
+        this.reader = reader;
+    }
+
+    public void kill()
+    {
+        continues = false;
+    }
