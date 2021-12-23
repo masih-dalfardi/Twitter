e791c2bdcbc976a4abdd5122f7ca6b75c09f689b
smaresalat <58745774+smaresalat@users.noreply.github.com>
2021-12-23 17:49:53 +0330

    [Feature] fields, constructors and methods added

--- Twitter/src/client/Client.java
+++ Twitter/src/client/Client.java
@@ -1,4 +1,47 @@
-package com.company.client;
+package client;
 
-public class Client {
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.Socket;
+
+public class Client
+{
+    private final int port = 8080;
+    private final String ip = "localhost";
+    private Socket socket;
+    private DataOutputStream writer;
+    private ObjectInputStream reader;
+
+    // a primary key for each client to connect it to its socket
+    private int connectionNumber;
+
+    public DataOutputStream getWriter()
+    {
+        return writer;
+    }
+
+    public ObjectInputStream getReader()
+    {
+        return reader;
+    }
+
+    // connecting to the server
+    public void connect() throws IOException, ClassNotFoundException
+    {
+        socket = new Socket(ip, port);
+        writer = new DataOutputStream(socket.getOutputStream());
+        reader = new ObjectInputStream(socket.getInputStream());
+
+        connectionNumber = (int) reader.readObject();
+    }
+
+    public int getNumber()
+    {
+        return connectionNumber;
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
 }
